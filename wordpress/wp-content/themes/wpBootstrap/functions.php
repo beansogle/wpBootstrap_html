6fffa7d83645e6be55183d20bc9fb34aeefa9ce6
Sean Bogle <snbgl22@gmail.com>
2017-05-02 19:30:30 -0400

    Added Featured Image stuff
    Included styling for the images.

--- wordpress/wp-content/themes/wpBootstrap/functions.php
+++ wordpress/wp-content/themes/wpBootstrap/functions.php
@@ -6,6 +6,8 @@ require_once('wp-bootstrap-navwalker.php');
     //theme support
 
     function wpb_theme_setup(){
+
+      add_theme_support('post-thumbnails');
       //nav menus
       register_nav_menus(array(
         'primary' => __('Primary Menu')
